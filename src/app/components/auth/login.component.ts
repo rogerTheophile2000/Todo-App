ba20747e5ac4e266196f985b95a0b16c4031ac10
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-07-14 19:28:28 +0300

    adding auth style and validators

--- src/app/components/auth/login.component.ts
+++ src/app/components/auth/login.component.ts
@@ -1,36 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import ToolbarComponent from '../shared/toolbar.component';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
   standalone: true,
   imports: [
     CommonModule,
     ToolbarComponent,
     ReactiveFormsModule,
-    RouterModule,
+    RouterModule
   ],
   template: `
     <app-toolbar [isLoginBtnShow]="true"></app-toolbar>
-    <form FormGroup="loginForm" class="form-container" (ngSubmit)="onSubmit()">
+    <form action="" [formGroup]="loginForm" class="form-container" (ngSubmit)="onSubmit()">
       <h2 class="title">Connectez-vous</h2>
       <h3 class="sub-title">
         Veuillez entrer votre email <a routerLink="/register">Enregistrez-vous</a>
       </h3><br>
       <input type="email" placeholder="Email" formControlName="email" />
-      <p **ngIf="showError">{{ errorMsg }}</p>
-      <button class="auth-btn" [disabled]="loginForm.invalid" type="submit">Connexion</button>
-    </form>
+      <p *ngIf="showError">{{ errorMsg }}</p>
+      <input type="password" placeholder="Mot de passe" formControlName="password" />
+      <button class="auth-btn" [ngClass]="{'active-btn' : !loginForm.invalid}" [disabled]="loginForm.invalid" type="submit" (click)="onSubmit()">Connexion</button>
+    </form> 
   `,
   styles: [
   ]
 })
-export default class LoginComponent {
+export default class RegisterComponent {
   showError = false;
-  errorMsg="Cet email n'existe pas, veuillez vous inscrire";
-  loginForm = new FormGroup({});
-  onSubmit(){}
+  errorMsg = "Email ou password incorect, veuillez vous inscrire";
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
+  onSubmit() { }
 }
--- src/app/components/auth/register.component.ts
+++ src/app/components/auth/register.component.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import ToolbarComponent from '../shared/toolbar.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ToolbarComponent],
+  imports: [
+    CommonModule,
+    ToolbarComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   template: `
     <app-toolbar [isRegisterBtnShow]="true"></app-toolbar>
-  `,
-  styles: [
-  ]
-})
-export default class RegisterComponent {
+    <form action="" [formGroup]="registerForm" class="form-container" (ngSubmit)="onSubmit()">
+      <h2 class="title">Connectez-vous</h2>
+      <h3 class="sub-title">
+        Veuillez entrer vos identifiants. <a routerLink="/login">Se Connecter</a>
+      </h3><br>
+      <input type="text" placeholder="Nom complet" formControlName="nomComplet">
+      <input type="email" placeholder="Email" formControlName="email" />
+      <input type="password" placeholder="Mot de passe" formControlName="password">
+      <button 
+        class="auth-btn"
+        [ngClass]="{'active-btn' : !registerForm.invalid}"
+        [disabled]="registerForm.invalid" type="submit"
+        (click)="onSubmit()">
+        S'inscrire
+      </button>
+    </form> 
 
+  `,
+  styles: [`
+    @use '../shared/_form.style.scss' as *;
+  `]
+})
+export default class LoginComponent {
+  registerForm = new FormGroup({
+    
+    nomComplet: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+  onSubmit() { }
 }
